264f999f0f4186e93928490ef34d673007b70292
tatytth <taty_tth@hotmail.com>
2017-10-06 10:45:32 -0300

    Actualización

--- cesarCipher/app.js
+++ cesarCipher/app.js
@@ -22,13 +22,21 @@ function cipher (){
 
 		for (var i =0; i < 26; i++){
 
-			var findLetter = txt.charCodeAt(i);				// Obtiene el código ASCII de las letras del texto.
-			findLetter = (((findLetter-65)+33)%26) + 65;	// Aplica la fórmula (x + n) % 26.
-			newTxt += String.fromCharCode(findLetter);		// Obtiene la letra en las posiciones halladas.
-				if (txt[i] == " "){
-					newTxt += " ";
+				var findLetter = txt.charCodeAt(i);						// Obtiene el código ASCII de las letras del texto.
+				if ((findLetter >= 65) && (findLetter <= 90)){			// Si son mayúsculas
+					findLetter = (((findLetter-65)+33)%26) + 65;		// Aplica la fórmula (x + n) % 26.
+					newTxt += String.fromCharCode(findLetter);			// Obtiene la letra en las posiciones halladas.
 				}
 
+				if ((findLetter >= 97) && (findLetter <= 122)){			// Si son minúsculas
+					findLetter = (((findLetter-97)+33)%26) + 97;		// Aplica la fórmula (x + n) % 26.
+					newTxt += String.fromCharCode(findLetter);			// Obtiene la letra en las posiciones halladas.
+				}														
+				
+				if (txt[i] == " "){										// Muestra los mismos espacios vacios
+					newTxt += " ";
+				}
+				
 		}
 		alert ("El texto cifrado es: " + newTxt);
 
@@ -41,13 +49,20 @@ function decipher (){
 
 		for (var j =0; j < 26; j++){
 
-			var findLetter = txt.charCodeAt(j);				// Obtiene el código ASCII de las letras del texto.
-			findLetter = (((findLetter-65)-7+52)%26+65) 	// Aplica la fórmula (x - n) % 26.
-			newTxt += String.fromCharCode(findLetter);		// Obtiene la letra en las posiciones halladas.
-				if (txt[j] == " "){
-					newTxt += " ";
+				var findLetter = txt.charCodeAt(j);						// Obtiene el código ASCII de las letras del texto.
+				if ((findLetter >= 65) && (findLetter <= 90)){			// Si son mayúsculas
+					findLetter = (((findLetter-65)-7+52)%26) + 65;		// Aplica la fórmula (x + n) % 26.
+					newTxt += String.fromCharCode(findLetter);			// Obtiene la letra en las posiciones halladas.
 				}
 
+				if ((findLetter >= 97) && (findLetter <= 122)){			// Si son minúsculas
+					findLetter = (((findLetter-97)-7+52)%26) + 97;		// Aplica la fórmula (x + n) % 26.
+					newTxt += String.fromCharCode(findLetter);			// Obtiene la letra en las posiciones halladas.
+				}
+				
+				if (txt[j] == " "){										// Muestra los mismos espacios vacios
+					newTxt += " ";
+				}	
 		}
 		alert ("El texto descifrado es: " + newTxt);
 
